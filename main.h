1aba7cfe49f1ae176abace420eec840423054db0
Rishabh Raj <rishabhraj240405@gmail.com>
2024-03-30 08:42:38 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	char str[100];
+	int i;
+
+	printf("Enter a value :");
+
+	scanf_s("%d %s", &i, str);
+
+	printf("\nYou entered: %d:::::%s\n", i, str);
+
+	return 0;
+}
+
